77d4693aabdf6f82ac2bcbe6ab333da2ff32dea4
sascha-lecours <sascha.lecours@gmail.com>
2021-07-02 13:44:59 -0400

    improving storysession model to pair with user documents

--- api/models/storySession.js
+++ api/models/storySession.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
 
 const storySessionSchema = mongoose.Schema({
-    // userID: { type: String }, // move to ID only
-    userName: { type: String, required: true },
+    userName: { type: String, required: true }, // TODO: this can be phased out to only use the objectID in "creator"
     storyName: { type: Object, required: true },
     sessionLastActiveStitch: { type: String },
-    sessionFlaglist: { type: Array, required: true }
-    
+    sessionFlaglist: { type: Array, required: true },
+    creator: { type: mongoose.Types.ObjectId, required: true, ref: 'User' },
 },
 { timestamps: true }
 );
